c8eb968de3d7dc048bc8037ea9824eb97153ef2b
brinleycrawley-del <brinleycrawley@gmail.com>
2026-03-24 12:09:21 -0500

    Implement click rotation effect on document body
    Added rotation functionality on click and removed console log.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 // main.js
+let rotation = 0;
+document.addEventListener("click", function (){
+  let min = 10;
+  let max = 40;
+  rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+  document.body.style.transform = `rotate(${rotation}deg)`;
+});
 
-console.log("Hello, World!");
-
-document.body.style.transform = 'rotate(90deg)';
-document.body.style.opacity = .5;
+function randomNumber(min, max){
+  return Math.floor(Math.random() * (max - min) + 1) + min;
+}
