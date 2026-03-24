958df9724d75ac92d4c4bad1b68e541dacfd3470
brinleycrawley-del <brinleycrawley@gmail.com>
2026-03-24 12:22:56 -0500

    Refactor rotation logic and add randomize function
    Removed click rotation functionality and added randomize function for divs.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,18 @@
 // main.js
-let rotation = 0;
-document.addEventListener("click", function (){
-  let min = 10;
-  let max = 40;
-  rotation += randomNumber(1, 200);
-  document.body.style.transform = `rotate(${rotation}deg)`;
-});
-
 function randomNumber(min, max){
   return Math.floor(Math.random() * (max - min) + 1) + min;
+}
+let divs = document.querySelectAll(".grid-container div");
+function randomize(){
+  divs.forEach(function(div){
+    let scale = randomNumber(.5, 1.2);
+    let translateX = randomNumber(0, 50);
+    let translateY = randomNumber(0, 20);
+    let rotate = randomNumber(0, 360);
+    div.style.transform = 
+      'scale(${scale})
+      translate(${translateX}%, ${translateY}%)
+  rotate(${rotate}deg)';
+  })
+  console.log(divs.length, "randomize!")
 }
