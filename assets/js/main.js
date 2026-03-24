8a6dbecece243950148683844e08f067faf1724f
brinleycrawley-del <brinleycrawley@gmail.com>
2026-03-24 12:10:05 -0500

    Change rotation calculation to use randomNumber function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ let rotation = 0;
 document.addEventListener("click", function (){
   let min = 10;
   let max = 40;
-  rotation += Math.floor(Math.random() * (max - min + 1)) + min;
+  rotation += randomNumber(1, 200);
   document.body.style.transform = `rotate(${rotation}deg)`;
 });
 
